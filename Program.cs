c88fb5fa3921de04378d0ad31e49ccd5d52cb09a
aiden-patterson <pat18001@byui.edu>
2022-09-20 15:53:53 -0600

    created initial dotnet project

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+// See https://aka.ms/new-console-template for more information
+namespace Test
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
